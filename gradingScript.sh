8e39d0d10f16b8cc9f79b48502c9306995f28cd0
Olufemi Salaam <salaam.olufemi@gmail.com>
2017-12-10 14:00:06 -0500

    Error in input fixed. Changed method of loading file

--- gradingScript.sh
+++ gradingScript.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-inputs=( 0ABcdE,FFfFFffF,000000000,1234@678,,"EA,","pqst,","123456789,",
+inputs=("0ABcdE","FFfFFffF","000000000","1234@678",,"EA,","pqst,","123456789,",
 "eA,",",pqst",",234567890"," ,",", "," , ","EA, ","pqst, ",
-"123456789,[space]","[space],eA",",pqst",",234567890",",,"," , , ",
+"123456789, "," ,eA",",pqst",",234567890",",,"," , , ",
 "0,9","0,012345678"," 7a3B4c6D , 12345678 ","12345678,f","f1,+",
 "***,F1","***,%%%","abcde12345,12345xyz","1234 abcd,12345abcd","123456789,abcdefabc",
 "0,9,A,a,F,f,10,1F,1f,F1,f1,EA,ea,eA,Ae,FF,ff,1234,7FFF,7fff,7ffF,FABC,FFFF,10000,12345,ABCDE,12345678,7A3B4C6D,7a3B4c6D,7FFFFFFF,87654321,8A6B7C5D,FFFFFFFF,00,00000000,000A,A000,0000A,A0000,00000ABC,4EF00000",
@@ -17,7 +17,8 @@ do
     
     for j in ${inputs[@]};
     do
-	echo $j | spim load $i
-	printf "\n"
+	    #echo $j | spim load $i
+        echo $j | spim run $i
+	    printf "\n"
     done
 done
